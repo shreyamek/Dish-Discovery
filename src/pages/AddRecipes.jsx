99929f62e78c2bf71568ebaecf79256f9fa6ffab
varshamall <106840434+varshamall@users.noreply.github.com>
2023-11-05 23:49:44 -0600

    Update AddRecipes.jsx

--- src/pages/AddRecipes.jsx
+++ src/pages/AddRecipes.jsx
@@ -1,32 +1,71 @@
 import React from 'react';
 
 export const AddRecipes = () => {
+  const containerStyle = {
+    backgroundColor: 'lightblue',
+    padding: '20px',
+    border: '1px solid lightblue',
+    borderRadius: '8px',
+  };
+
+  const outerContainerStyle = {
+    backgroundColor: 'lightgrey',
+    padding: '10px',
+    border: '1px solid grey',
+    borderRadius: '8px',
+  };
+
+  const lineStyle = {
+    backgroundColor: 'lightblue',
+    height: '2px',
+    margin: '15px 0',
+  };
+
+  const submitButtonStyle = {
+    backgroundColor: '#5A5A5A',
+    border: '1px solid grey',
+    padding: '15px 30px', // Larger padding for size
+    color: 'white',
+    borderRadius: '8px',
+    fontSize: '20px', // Larger font size
+  };
+
   return (
     <div>
-      <h2>Add Your Recipe</h2>
-      <p>
-        Share your favorite recipe with the community! Fill out the form below to 
-        add your recipe to our collection.
-      </p>
+      <div style={outerContainerStyle}>
+        <h2>Add Your Recipe</h2>
+        <p>
+          Share your favorite recipe with the community! Fill out the form below to 
+          add your recipe to our collection.
+        </p>
 
-      <form>
-        <div>
-          <label htmlFor="recipeName">Recipe Name:</label>
-          <input type="text" id="recipeName" name="recipeName" />
+        <div style={containerStyle}>
+          <form>
+            <div>
+              <label htmlFor="recipeName">Recipe Name:</label>
+              <input type="text" id="recipeName" name="recipeName" />
+            </div>
+            <div style={lineStyle}></div>
+            <div>
+              <label htmlFor="ingredients">Ingredients:</label>
+              <textarea id="ingredients" name="ingredients"></textarea>
+            </div>
+            <div style={lineStyle}></div>
+            <div>
+              <label htmlFor="instructions">Instructions:</label>
+              <textarea id="instructions" name="instructions"></textarea>
+            </div>
+            <div style={lineStyle}></div>
+            <button style={submitButtonStyle} type="submit">
+              SUBMIT RECIPE
+            </button>
+          </form>
         </div>
-
-        <div>
-          <label htmlFor="ingredients">Ingredients:</label>
-          <textarea id="ingredients" name="ingredients"></textarea>
-        </div>
-
-        <div>
-          <label htmlFor="instructions">Instructions:</label>
-          <textarea id="instructions" name="instructions"></textarea>
-        </div>
-
-        <button type="submit">Submit Recipe</button>
-      </form>
+      </div>
     </div>
   );
 }
+
+
+
+

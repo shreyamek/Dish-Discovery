c436838b5dcd20195d18a964f2291ff28b1a560a
varshamall <106840434+varshamall@users.noreply.github.com>
2023-11-05 23:31:39 -0600

    Update AddRecipes.jsx

--- src/pages/AddRecipes.jsx
+++ src/pages/AddRecipes.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 export const AddRecipes = () => {
   return (
-    <div>AddRecipes</div>
-  )
+    <div>
+      <h2>Add Your Recipe</h2>
+      <p>
+        Share your favorite recipe with the community! Fill out the form below to 
+        add your recipe to our collection.
+      </p>
+
+      <form>
+        <div>
+          <label htmlFor="recipeName">Recipe Name:</label>
+          <input type="text" id="recipeName" name="recipeName" />
+        </div>
+
+        <div>
+          <label htmlFor="ingredients">Ingredients:</label>
+          <textarea id="ingredients" name="ingredients"></textarea>
+        </div>
+
+        <div>
+          <label htmlFor="instructions">Instructions:</label>
+          <textarea id="instructions" name="instructions"></textarea>
+        </div>
+
+        <button type="submit">Submit Recipe</button>
+      </form>
+    </div>
+  );
 }

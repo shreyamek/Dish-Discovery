41553b2f6240eb94fbf74a0861efedbf022cdc7c
Laya Vadapalli <72660599+layavadapalli@users.noreply.github.com>
2023-11-18 15:20:27 -0600

    updated profile and edit profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import background from '../BackgroundImage.png';
 import circle from '../ProfilePic.png';
 import './Profile.css';
-import { useNavigate} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 //const [activeIndex, setActiveIndex] = useState(0);
 
@@ -14,31 +14,45 @@ const user = {
 //Laya
 export const Profile = () => {
 
-  const navigate = useNavigate;
-  const handleclick = () => {
-    navigate(`/editprofile`)
-  }
-
   return (
     <div>
       <div className="App" style={{backgroundImage: `url(${background})`}}>
         <br/><br/>
-        <img
-          className="avatar"
-          src={circle}
-          alt={'Photo of ' + user.name}
-          style={{
-            width: user.imageSize,
-            height: user.imageSize
-
-          }}
-        />
-        <h2>{user.name}</h2>
-       <button onClick={handleclick}>
-          edit profile
-        </button>
-      </div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <img
+        className="avatar"
+        src={circle}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
     </div>
+        <h2>{user.name}</h2>
+        <div className='container2'>
+          <div className="editButton">
+            <Link to="/EditProfile" className="link"style={{ color: 'black'}}> Edit Profile </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="rectangleBar">
+        <div className="head2"> Bookmarks </div>
+      </div>
+      <div className="rectangleBar2">
+        <div className="head2"> Your Recipies </div>
+      </div>
+      <div className="rectangleBar3">
+        <div className="head2"> Folders </div>
+      </div>
+      <div className="rectangleBar4">
+        <div className="head2"> About Me </div>
+      </div>
+
+    </div>
+
+    
       
 
   )

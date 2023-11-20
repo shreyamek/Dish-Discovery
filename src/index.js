bed08771c53646e806fd360657e5390c17c73e9d
varshamall <106840434+varshamall@users.noreply.github.com>
2023-11-19 18:07:18 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { RecipeProvider } from './pages/RecipeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,4 +12,13 @@ root.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RecipeProvider>
+      <App />
+    </RecipeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );

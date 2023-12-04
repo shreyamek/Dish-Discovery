5b1a5def3ea928fe4ef24be452a4c8e31c007e32
varshamall <106840434+varshamall@users.noreply.github.com>
2023-12-04 02:44:02 -0600

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,13 @@
-import background from '../BackgroundImage.png';
-import circle from '../ProfilePic.png';
-import './Profile.css';
-import { Link } from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
-import { useRecipeContext } from '../pages/RecipeContext';
-import { useProfile } from './ProfileContext';
+import background from "../BackgroundImage.png";
+import circle from "../ProfilePic.png";
+import "./Profile.css";
+import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useRecipeContext } from "../pages/RecipeContext";
+import { useProfile } from "./ProfileContext";
 
 const user = {
-  name:''
+  name: "",
 };
 
 //Laya
@@ -16,34 +16,54 @@ export const Profile = () => {
   const { profileData } = useProfile();
 
   const recipeBoxStyle = {
-    background: '#E0E0E0', 
-    padding: '10px',
-    margin: '10px 0',
-    borderRadius: '10px',
-   
+    background: "#E0E0E0",
+    padding: "10px",
+    margin: "10px 0",
+    borderRadius: "10px",
+    marginLeft: "20px",
+    marginRight: "20px",
+    display: "flex",
+    flexDirection: "column",
+  };
+
+  const recipeImageStyle = {
+    width: "300px",
+    height: "auto", 
+    objectFit: "cover",
+    borderRadius: "5px",
+    alignSelf: "center",  
   };
 
   return (
     <div>
-      <div className="App" style={{backgroundImage: `url(${background})`}}>
-        <br/><br/>
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <img
-        className="avatar"
-        src={circle}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: 300,
-          height: 300,
-        }}
-      />
-    </div>
-    <p>{profileData.userName}</p>
-        <div className='container2'>
-        <div className="editButton">
-              <Link to="/EditProfile" className="link"style={{ color: 'black'}}> Edit Profile </Link>
+      <div className="App" style={{ backgroundImage: `url(${background})` }}>
+        <br />
+        <br />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            className="avatar"
+            src={circle}
+            alt={"Photo of " + user.name}
+            style={{
+              width: 300,
+              height: 300,
+            }}
+          />
         </div>
-
+        <p>{profileData.userName}</p>
+        <div className="container2">
+          <div className="editButton">
+            <Link to="/EditProfile" className="link" style={{ color: "black" }}>
+              {" "}
+              Edit Profile{" "}
+            </Link>
+          </div>
         </div>
       </div>
 
@@ -56,13 +76,18 @@ export const Profile = () => {
           {recipes.map((recipe, index) => (
             <li key={index} style={recipeBoxStyle}>
               <strong>{recipe.recipeName}</strong>
+              <img
+                src={recipe.recipeImage}
+                alt={`Image for ${recipe.recipeName}`}
+                style={recipeImageStyle}
+              />
               <ul>
                 {recipe.ingredients.map((ingredient, ingIndex) => (
                   <li key={ingIndex}>{ingredient}</li>
                 ))}
               </ul>
               <p>
-                {recipe.instruction.split('\n').map((line, lineIndex) => (
+                {recipe.instruction.split("\n").map((line, lineIndex) => (
                   <span key={lineIndex}>
                     {line}
                     <br />
@@ -78,8 +103,8 @@ export const Profile = () => {
       </div>
       <div className="rectangleBar4">
         <div className="head2"> About Me </div>
-          <p>Here's a little about me: {profileData.aboutMe}</p>
+        <p>Here's a little about me: {profileData.aboutMe}</p>
       </div>
     </div>
-  )
-}
+  );
+};
